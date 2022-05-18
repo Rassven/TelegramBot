61bd811791cd49f81a5aa12474efc77db74ad9a4
Rassven <103177474+Rassven@users.noreply.github.com>
2022-05-19 02:26:08 +0800

    Update tbot_app.py
    Вылечены: обработчики ввода и исключений. Собран в целое сборщик ответа.

--- tbot_app.py
+++ tbot_app.py
@@ -1,40 +1,51 @@
-﻿import requests
+import requests
 import json
-from tbot_config import ru_c_base, inter_c_base, ru_err_mess, ru_c_dict
-base = ''  #русские базовые
-amount = ''  #текст
-target = ''  #русские базовые
+from tbot_config import ru_c_dict, ru_err_mess, ru_greeting_mess
+from tbot_main import bot
+err_message = ''
 
-class InputException(Exception):
-   def __str__(self):
-       pass
+
+class UserException(Exception):
+   def __init__(self, r_str):
+       global err_message
+       err_message = 'Ошибка!' + r_str
 
 
 class Converter:
     @staticmethod
     def convert(pars):
-        #tst_str = 'перевести 129 биткоинов в доллары или евро или рубли'; pars = tst_str
-
-        words = pars.split(' ')
+        #tst_str = 'эфириум перевести в 129 биткоинов и доллары или евро или рубли'; pars = tst_str
+        
+        global err_message, base, target, amount
+        if pars == '':  #команда mem
+            pars = base + ' ' + target + ' ' + amount
+        words = pars.split()
         fpar = []; num = -1
         for wcou in words:
             if wcou.isnumeric():
                 num = float(wcou)
                 break
-        for checkval in ru_c_base:
-            for wcou in words:
-                if wcou.find(checkval) > -1:
-                    fpar.append(checkval)
+        for wcou in words:
+            for check_val in ru_c_dict:
+                if wcou.find(check_val) > -1:
+                    fpar.append(check_val)
                     break
 
         #Обработка поиска
+        if len(words) > 3 and len(fpar) == 0:
+            bot.reply_to(message, ru_greeting_mess[2])
         try:
             if len(fpar) == 0 and num == -1:
-                raise InputException('Ошибка.' + ru_err_mess.get('input')[0])
+                raise UserException(ru_err_mess.get('input')[0])
             if len(fpar) > 2:
-                raise InputException('Ошибка.' + ru_err_mess.get('input')[1])
-        except InputException as e:
-            pass
+                raise UserException(ru_err_mess.get('input')[1])
+        except UserException:
+            bot.reply_to(message, err_message)
+        try:
+            if amount == '' and num < 0:
+                raise UserException(ru_err_mess.get('Input')[4])
+        except UserException:
+            bot.reply_to(message, err_message)
 
         #Обработка данных
         if num > -1:
@@ -42,18 +53,22 @@ class Converter:
         try:
             if len(fpar) == 1:
                 if base == fpar[0]:
-                    raise InputException('Ошибка.' + ru_err_mess.get('conversion')[0])
+                    raise UserException(ru_err_mess.get('conversion')[0])
                 else:
                     target = fpar[0]
             else:
                 base = fpar[0]
                 target = fpar[1]
-        except InputException as e:
-            pass
-
-        base_ticker = inter_c_base[ru_c_base.index(base)]
-        target_ticker = inter_c_base[ru_c_base.index(target)]
-
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={target_ticker}&tsyms={base_ticker}')
-        result = float(amount) * float(json.loads(r.content)[ru_c_dict[base]])
-        return result
+        except UserException:
+            bot.reply_to(message, err_message)
+        
+        #сборка
+        base_ticker = ru_c_dict[base][0]
+        target_ticker = ru_c_dict[target][0]
+        
+        #Запрос: fsym=USD&tsyms=RUB, ответ: {"RUB": 63.93}
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={target_ticker}')
+        result = float(amount) * json.loads(r.content)[target_ticker]
+        ending = 2
+        result_text = f'Стоимость {amount} {base + ru_c_dict[base][ending]} составляет {result} {target + ru_c_dict[target][ending]}'
+        return result_text
